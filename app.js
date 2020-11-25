0c28d79afb044a8fef4050511f7f4d10ee2fae8a
EC2 Default User <ec2-user@ip-172-31-11-67.us-west-1.compute.internal>
2020-11-25 07:48:31 +0000

    add faker

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express = require("express");
 const app = express();
 app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
+var faker = require('faker');
 
 //routes
 app.get("/", function(req, res){
-    res.render("index.html");
+    res.render("index.html", {fakeInfo: faker.lorem.paragraph()
+    });
 });
 
 
 app.get("/data", function(req, res){
-    res.render("data.html");
+    res.render("data.html",{fakeInfo: faker.lorem.paragraph()
+    });
 });
 
 
@@ -19,7 +22,8 @@ app.get("/application", function(req, res){
 });
 
 app.get("/storage", function(req, res){
-    res.render("storage.html");
+    res.render("storage.html",{fakeInfo: faker.lorem.paragraph()
+    });
 });
 
 //starting server
